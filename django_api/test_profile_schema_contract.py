2b5f46aa629f3e9a873cfbfc8806114ece8bdb37
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2026-09-17 17:13:11 +0530

    test: lock student profile OpenAPI contract to serializer

---
+++ django_api/test_profile_schema_contract.py
@@ -0,0 +1,22 @@
+from django.test import SimpleTestCase
+from drf_spectacular.generators import SchemaGenerator
+
+from django_api.serializers import ProfileCreateUpdateSerializer
+
+
+class StudentProfileSchemaContractTests(SimpleTestCase):
+    def test_profile_openapi_fields_match_serializer_exactly(self):
+        schema = SchemaGenerator().get_schema(request=None, public=True)
+        operation = schema["paths"]["/api/profile/"]["post"]
+        request_schema = operation["requestBody"]["content"]["application/json"]["schema"]
+        component_name = request_schema["$ref"].rsplit("/", 1)[-1]
+        component = schema["components"]["schemas"][component_name]
+
+        documented_fields = set(component.get("properties", {}))
+        serializer_fields = set(ProfileCreateUpdateSerializer().fields)
+
+        self.assertEqual(documented_fields, serializer_fields)
+        self.assertNotIn("dateOfBirth", documented_fields)
+        self.assertNotIn("yearInCollege", documented_fields)
+        self.assertIn("date_of_birth", documented_fields)
+        self.assertIn("year_in_college", documented_fields)
