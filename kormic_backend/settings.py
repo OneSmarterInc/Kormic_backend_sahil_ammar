057b2245bbceaca25debf3b709cef26d923a5eb6
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2026-09-22 15:36:54 +0530

    Rotate and blacklist refresh tokens

--- kormic_backend/settings.py
+++ kormic_backend/settings.py
@@ -330,8 +330,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
     "USER_ID_FIELD": "id",
     "USER_ID_CLAIM": "user_id",
